3f1f0107c760d963b017fbc58f7c8f85673f9ffa
AdharshG <34604785+Adharshg@users.noreply.github.com>
2018-08-14 23:39:25 +0530

    Basic prototype
    Made maze
    movement + camera rotation sync (Yay!)
    Changed directional lighting
    Made my character
    
    Problem :
    Cube getting tilted with camera
    mechanics fixing
    
    To do:
    Asset creation - collectables, enemies
    Ideas for making it look interesting
    Information and writing boards
    Music and SFX
    etc.

---
+++ CTM/Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontroller : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	public bool Rotatearoundplayer = true;
+
+	public float RotationSpeed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if(Rotatearoundplayer){
+			Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+
+			offset = camTurnAngle * offset;
+		}
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ CTM/Assets/Scripts/Hercontrol.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+
+public class Hercontrol : MonoBehaviour {
+
+	//public GameObject Winscreen;	
+	private Rigidbody rb;
+	public float speed=100f;
+	private int count;
+	public GameObject mainCamera;
+	//public Text countText;
+	//public Text wintext;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		isMoving = true;
+		//count = 0;
+		//SetCountText();
+		//wintext.text="";
+	}
+
+	void FixedUpdate(){
+		Vector3 fromCameraToMe = transform.position - mainCamera.transform.position;
+		fromCameraToMe.y = 0;
+		fromCameraToMe.Normalize();
+		float moveHorizontal=Input.GetAxis("Horizontal");
+		float moveVertical=Input.GetAxis("Vertical");
+
+		Vector3 movement = (fromCameraToMe * moveVertical + mainCamera.transform.right * moveHorizontal) * speed;
+
+		rb.AddForce(movement * speed);
+	}
+	/* 
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("pick up")){
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = count.ToString() + "/32 COINS";
+		if(count>=1){
+			wintext.text = "YOU WIN !!";
+			countText.text="";
+			Time.timeScale = 0f;
+			Winscreendisp();
+		}
+	}
+
+	public void Winscreendisp(){
+		Winscreen.SetActive(true);
+		gameObject.SetActive(false);
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene(1);
+	}
+	*/
+}
